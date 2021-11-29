380b990f8418a30e921692e3d5538c45b2e069cc
Zboru <zboru99@gmail.com>
2021-11-29 10:24:40 +0100

    create step2

--- pages/app.js
+++ pages/app.js
@@ -1,9 +1,60 @@
+import {useState} from "react";
 import Step1 from "../components/steps/Step1";
+import Step2 from "../components/steps/Step2";
+import subDays from "date-fns/subDays";
 
 export default function App() {
+    const [selectedRadio, selectRadio] = useState(null);
+    const [currentStep, setStep] = useState(0);
+    const [timeRange, setTimeRange] = useState({
+        startDate: subDays(new Date(), 7),
+        startHour: 7,
+        endDate: new Date(),
+        endHour: 15,
+    });
+
+    function moveForward() {
+        if (currentStep < 5) {
+            setStep(currentStep + 1);
+        }
+    }
+
+    function moveBackward() {
+        if (currentStep > 0) {
+            setStep(currentStep - 1);
+        }
+    }
+
+    function setStartDate(...args) {
+        setTimeRange({...timeRange, startDate: args[0]})
+    }
+
+    function setEndDate(...args) {
+        setTimeRange({...timeRange, endDate: args[0]})
+    }
+
+    function setHours(hours) {
+        setTimeRange({...timeRange, startHour: hours[0], endHour: hours[1]})
+    }
+
     return (
         <>
-            <Step1 active={true}/>
+            <Step1 selectedRadio={selectedRadio}
+                   selectRadio={selectRadio}
+                   onForward={moveForward}
+                   active={currentStep === 0}
+            />
+            <Step2 onForward={moveForward}
+                   onBackward={moveBackward}
+                   active={currentStep === 1}
+                   startDate={timeRange.startDate}
+                   endDate={timeRange.endDate}
+                   startHour={timeRange.startHour}
+                   endHour={timeRange.endHour}
+                   onHourChange={setHours}
+                   onStartDateChange={setStartDate}
+                   onEndDateChange={setEndDate}
+            />
         </>
     )
 }
---
+++ utils/day-picker.js
@@ -0,0 +1,23 @@
+export const MONTHS = [
+    'Styczeń',
+    'Luty',
+    'Marzec',
+    'Kwiecień',
+    'Maj',
+    'Czerwiec',
+    'Lipiec',
+    'Sierpień',
+    'Wrzesień',
+    'Październik',
+    'Listopad',
+    'Grudzień'
+];
+export const WEEKDAYS_SHORT = [
+    'Pon',
+    'Wt',
+    'Śr',
+    'Czw',
+    'Pt',
+    'Sob',
+    'Nd'
+]
