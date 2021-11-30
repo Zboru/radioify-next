17aa5a6493ba81fad57fa909634173ef0bd7b600
Zboru <zboru99@gmail.com>
2021-11-30 10:28:12 +0100

    small fixes in Step6

--- components/steps/Step6.js
+++ components/steps/Step6.js
@@ -6,13 +6,15 @@ import {HomeIcon} from "../icons/HomeIcon";
 export default function Step6({active}) {
     return (
         <Card active={active}>
-            <p>Gratulacje! Dodano utwory z radia do Twojej playlisty! <br/> Przejdź do platformy Spotify i ciesz się muzyką
-                wolną od reklam radiowych.</p>
+            <p>Gratulacje! Dodano utwory z radia do Twojej playlisty! <br/> Przejdź do platformy Spotify i ciesz się
+                muzyką wolną od reklam radiowych.</p>
             <Link href="/">
+                <a>
                     <Btn className="mt-2 flex">
                         <HomeIcon className="mr-2 text-xl"/>
                         <span>Powrót do strony głównej</span>
                     </Btn>
+                </a>
             </Link>
         </Card>
     )
